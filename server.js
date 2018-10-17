052fc6e393b0b7deb60013538e124d3cceafa60a
Hanzou Hattori <hanzou.fuuma@gmail.com>
2018-10-17 04:50:19 +0000

    Use a helper function to avoid copypasting those three lines

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ var app = express();
 
 var cached_amount = 0.0;
 
+var reportSupply = function(res, amount) {
+    console.log('total supply: ' + amount);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(''+amount);
+};
+
 app.get('/chain/FlappyCoin/q/totalbc', function(req, res) {
     var process = require('child_process');
     console.log('GET from ' + req.connection.remoteAddress);
     if (cached_amount != 0.0) {
-        console.log('total supply: ' + cached_amount);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(''+cached_amount);
+        reportSupply(res, cached_amount);
     }
     process.exec('../Flaps/src/flappycoind gettxoutsetinfo', function(err, stdout, stderr) {
         if (err) {
@@ -26,9 +30,7 @@ app.get('/chain/FlappyCoin/q/totalbc', function(req, res) {
             var corrected_amount = signed_amount + (Math.pow(2, 64) / 100000000.0);
 
             if (cached_amount == 0.0) {
-                console.log('total supply: ' + corrected_amount);
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(''+corrected_amount);
+                reportSupply(res, corrected_amount);
             }
             cached_amount = corrected_amount;
         }
